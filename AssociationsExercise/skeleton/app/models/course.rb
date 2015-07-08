6bd2c4b17af272b5681310dc3340a64a75e1d2aa
Vishak Visvanathan <vishak.visvanathan@gmail.com>
2015-07-08 11:57:30 -0700

    finish associations exercise

--- AssociationsExercise/skeleton/app/models/course.rb
+++ AssociationsExercise/skeleton/app/models/course.rb
@@ -1,2 +1,28 @@
 class Course < ActiveRecord::Base
+  has_many(
+    :enrollments,
+    class_name: "Enrollment",
+    primary_key: :id,
+    foreign_key: :course_id
+  )
+
+  has_one(
+    :prerequisite,
+    class_name: "Course",
+    primary_key: :prereq_id,
+    foreign_key: :id
+  )
+
+  has_one(
+    :instructor,
+    class_name: "User",
+    primary_key: :instructor_id,
+    foreign_key: :id
+  )
+
+  has_many(
+    :enrolled_students,
+    through: :enrollments,
+    source: :user
+  )
 end
